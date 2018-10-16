ffe38c06e92f3afb712119aa8ca34ed354449cf9
Eric <kizu@kizutsu-2.local>
2018-10-16 13:35:28 +0200

    Changed communication. Implemented task 1: asymmetric encryption to send symmetric key.

--- Receiver.java
+++ Receiver.java
@@ -1,18 +1,78 @@
+import java.io.*;
 import java.net.*;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 
 //'server'
 public class Receiver {
 	private static ServerSocket serverSocket;
+	private static DataInputStream dataIn;
+	private static DataOutputStream dataOut;
+	private static SecretKey symmetricKey;
 	
 	public static void main(String[] args) throws Exception{
 		System.out.println("Server has started");
-		
 		serverSocket = new ServerSocket(1024);
 		
-		while(true) {
-			Socket socket = null;
-			socket = serverSocket.accept();
-		}
+		System.out.println("Waiting for sender...");
+		Socket socket = serverSocket.accept();
+		dataIn = new DataInputStream(socket.getInputStream());
+		dataOut = new DataOutputStream(socket.getOutputStream());
+		System.out.println("Sender connected.");
+//		MessageListener listener = new MessageListener(socket);
+//		listener.start();
+		
+		System.out.println("Starting generation of symmetric key.");
+		symmetricKey = generateKey(); //symmetric key
+		String symmetricKeyString = keyToString(symmetricKey);
+		
+		System.out.println("Waiting for public key to encrypt and send the symmetric key...");
+		String publicKeyString = dataIn.readUTF();
+		PublicKey pubKey = stringToPublicKey(publicKeyString);
+		System.out.println("Received public key");
+		
+		System.out.println("Encrypting symmetric key with the public key.");
+		byte[] encryptedSymKeyString = encryptAsymmetric(pubKey, symmetricKeyString.getBytes());
+		System.out.println("Sending encrypted symmetric key.");
+		//System.out.println("TEST: encrypted symmetric key: "+new String(encryptedSymKeyString)); //testprint
+		dataOut.writeInt(encryptedSymKeyString.length);
+		dataOut.write(encryptedSymKeyString);
+		
+		//otherstuff
+	}
+	
+	public static SecretKey generateKey() throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        SecretKey key = keyGenerator.generateKey();
+        return key;
+    }
+	
+    public static byte[] encryptAsymmetric(/*byte[] publicKey,*/ PublicKey key, byte[] inputData) throws Exception {
+		//PublicKey key = KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(publicKey));
+
+	    Cipher cipher = Cipher.getInstance("RSA");
+	    cipher.init(Cipher.PUBLIC_KEY, key);
+	
+	    byte[] encryptedBytes = cipher.doFinal(inputData);
+	
+	    return encryptedBytes;
+    }
+    
+    public static PublicKey stringToPublicKey(String key) throws Exception {
+		byte[] stringBytes = Base64.getDecoder().decode(key);
+		return KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(stringBytes));
+    }
+	
+	public static String keyToString(SecretKey secretKey) {
+		return Base64.getEncoder().encodeToString(secretKey.getEncoded());
 	}
 }
 
--- Sender.java
+++ Sender.java
@@ -1,22 +1,72 @@
 import java.io.*;
 import java.net.Socket;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 //'terminal client'
 public class Sender {
 	private static Socket socket;
+	private static DataInputStream dataIn;
 	private static DataOutputStream dataOut;
+	private static SecretKey symmetricKey;
 	
 	public static void main(String[] args) throws Exception{
 		System.out.println("Sender has started.");
 		System.out.println("Connecting to 0.0.0.0 port 1024");
 		socket = new Socket("0.0.0.0", 1024);
+		dataIn = new DataInputStream(socket.getInputStream());
 		dataOut = new DataOutputStream(socket.getOutputStream());
 		System.out.println("Connected to socket.");
 		
-		MessageListener listener = new MessageListener(socket);
-		listener.start();
+		//Generating asymmetric keys
+		System.out.println("Starting asymmetric encryption...");
+		System.out.println("Generating public and private keys");
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+		keyPairGenerator.initialize(512); //Number of bits in both keys
+		KeyPair keyPair = keyPairGenerator.generateKeyPair();
+		PublicKey pubKey = keyPair.getPublic();    //This is the public key
+		PrivateKey privKey = keyPair.getPrivate(); //This is the private key
 		
+		System.out.println("Sending public key...");
+		String publicKeyString = keyToString(pubKey);
+		dataOut.writeUTF(publicKeyString);
 		
+		System.out.println("Waiting to receive encrypted symmetric key...");
+		int encrSymKeyLength = dataIn.readInt();
+		byte[] encryptedSymKeyString = new byte[encrSymKeyLength];
+		dataIn.readFully(encryptedSymKeyString, 0, encrSymKeyLength);
+		System.out.println("Encrypted symmetric key received.");
+		//System.out.println("TEST: encrypted symmetric key: "+new String(encryptedSymKeyString)); //testprint
+		
+		String symKeyString = new String(decryptAsymmetric(privKey, encryptedSymKeyString));
+		symmetricKey = stringToSecretKey(symKeyString);
+		System.out.println("Decrypted symmetric key.");
 	}
 	
+    public static byte[] decryptAsymmetric(/*byte[] privateKey,*/ PrivateKey key, byte[] inputData) throws Exception {
+        //PrivateKey key = KeyFactory.getInstance("RSA").generatePrivate(new PKCS8EncodedKeySpec(privateKey));
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.PRIVATE_KEY, key);
+
+        byte[] decryptedBytes = cipher.doFinal(inputData);
+
+        return decryptedBytes;
+    }
+    
+    public static SecretKey stringToSecretKey(String encodedKey) {
+		byte[] decodedKey = Base64.getDecoder().decode(encodedKey);
+		return new SecretKeySpec(decodedKey, 0, decodedKey.length, "AES");
+    }
+	
+	public static String keyToString(PublicKey key) {
+		return Base64.getEncoder().encodeToString(key.getEncoded());
+	}
 }
